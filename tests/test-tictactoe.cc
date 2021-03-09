6e7d2f43e67c8b656a60bf6e283f7dff2fccc34a
Saad Sadiq <ssadiq8@illinois.edu>
2021-03-09 16:21:42 -0600

    Wrote first 3 categories of tests, need to add unreachable tests

--- tests/test-tictactoe.cc
+++ tests/test-tictactoe.cc
@@ -11,17 +11,77 @@ TEST_CASE("Invalid string provided to constructor") {
     REQUIRE_THROWS_AS(Board("xxooo"), std::invalid_argument);
   }
 
-  SECTION("placeholder") {
+  SECTION("String is empty") {
+    REQUIRE_THROWS_AS(Board(""), std::invalid_argument);
+  }
 
+  SECTION("String is too long") {
+    REQUIRE_THROWS_AS(Board("XoX..oXooX"), std::invalid_argument);
   }
 }
 
 TEST_CASE("Boards with no winner") {
-  SECTION("Full board with no winner") {
+  SECTION("Full 3x3 board with no winner") {
     REQUIRE(Board("xxoooxxxo").EvaluateBoard() == BoardState::NoWinner);
   }
 
-  SECTION("placeholder") {
+  SECTION("3x3 game in progress x went last") {
+    REQUIRE(Board("..O.x.X..").EvaluateBoard() == BoardState::NoWinner);
+  }
 
+  SECTION("3x3 game in progress o went last") {
+    REQUIRE(Board("..O.x.X..").EvaluateBoard() == BoardState::NoWinner);
+  }
+
+  SECTION("Full 4x4 board with no winner") {
+    REQUIRE(Board("xxooooxxxxooooxx").EvaluateBoard() == BoardState::NoWinner);
+  }
+
+  SECTION("Empty Board State") {
+    REQUIRE(Board(".........").EvaluateBoard() == BoardState::NoWinner);
+  }
+}
+
+TEST_CASE("Valid Boards X Wins") {
+  SECTION("3x3 board vertical win") {
+    REQUIRE(Board(".xoOxo.x.").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("3x3 board horizontal win") {
+    REQUIRE(Board("o.oxXx.s3").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("3x3 board diagonal win") {
+    REQUIRE(Board("xo..xo.Ox").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("3x3 board anti-diagonal win") {
+    REQUIRE(Board(".ox.Xox.o").EvaluateBoard() == BoardState::Xwins);
+  }
+
+  SECTION("4x4 board horizontal win") {
+    REQUIRE(Board("xXxxO.o.O.o..O.x").EvaluateBoard() == BoardState::Xwins);
+  }
+}
+
+TEST_CASE("Valid Boards O wins") {
+  SECTION("3x3 board vertical win") {
+    REQUIRE(Board("x.ox.o.xo").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("3x3 board horizontal win") {
+    REQUIRE(Board("oOox.x.sx").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("3x3 board diagonal win") {
+    REQUIRE(Board("ox..ox.Xo").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("3x3 board anti-diagonal win") {
+    REQUIRE(Board(".Xo.oXo.x").EvaluateBoard() == BoardState::Owins);
+  }
+
+  SECTION("4x4 board diagonal win") {
+    REQUIRE(Board("Ox..Xo.x..o..x.O").EvaluateBoard() == BoardState::Owins);
   }
 }
